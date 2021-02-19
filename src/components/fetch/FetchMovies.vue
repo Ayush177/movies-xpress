8e2d04f4a3a75be0c3e705abf35211669f518144
Ayush177 <ayusharora17785@gmail.com>
2021-02-19 15:24:32 +0530

    fixed search button

--- src/components/fetch/FetchMovies.vue
+++ src/components/fetch/FetchMovies.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <b-row>
-            <b-col cols="12" md="11" lg="11">
+            <b-col cols="12" md="11" lg="11"  class="p-2">
                 <b-form-input v-model="ids" placeholder="Enter IMDB ids separated by commas" @keydown.enter="collectIDs"></b-form-input>
             </b-col>
-            <b-col cols="12" md="1" lg="1" class="text-center">
+            <b-col cols="12" md="1" lg="1" class="text-center p-2">
                 <b-button variant="primary" @click="collectIDs">Search</b-button>
             </b-col>
         </b-row>
